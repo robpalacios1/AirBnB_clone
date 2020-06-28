0662c6570a7d847e306399fe2eb8371bc1137553
Roberto <robpalacios1@hotmail.com>
2020-06-28 17:41:00 -0500

    update base_model.py file

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/Usr/Bin/python3
 '''module 'base_model'''
 
 
@@ -10,20 +10,32 @@ import models
 
 class BaseModel():
     '''class BaseModel'''
-    def __init__(self, id=None):
+    def __init__(self, *args, **kwargs):
         '''class constructor for class BaseModel'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         '''string of BaseModel instance'''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         '''updates 'updated_at' instance with current datetime'''
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         '''dictionary representation of instance'''
