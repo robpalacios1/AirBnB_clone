aad4281e720d642e425c4a404fbd766b14eba218
hfsantiago <santiquiu@gmail.com>
2020-06-28 16:42:02 -0500

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/Usr/Bin/python3
 '''module 'base_model'''
 
 
@@ -10,16 +10,26 @@ import models
 
 class BaseModel():
     '''class BaseModel'''
-    def __init__(self, id=None):
+    def _init_(self, *args, **kwargs):
         '''class constructor for class BaseModel'''
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
 
-    def __str__(self):
+            for key, value in kwargs.items():
+                if key != '_class_':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def _str_(self):
         '''string of BaseModel instance'''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self._class.name_,
+                                     self.id, self._dict_)
 
     def save(self):
         '''updates 'updated_at' instance with current datetime'''
@@ -27,8 +37,8 @@ class BaseModel():
 
     def to_dict(self):
         '''dictionary representation of instance'''
-        new_dict = dict(self.__dict__)
-        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
-        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
-        new_dict['__class__'] = self.__class__.__name__
+        new_dict = dict(self._dict_)
+        new_dict['created_at'] = self._dict_['created_at'].isoformat()
+        new_dict['updated_at'] = self._dict_['updated_at'].isoformat()
+        new_dict['_class'] = self.class.name_
         return (new_dict)
