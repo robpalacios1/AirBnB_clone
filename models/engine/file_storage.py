7269c5dd294b9933ea71deca7abf845980d4ae67
Roberto <robpalacios1@hotmail.com>
2020-06-28 17:41:46 -0500

    update file_storage.py file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''A class Filestorage that serializes instances to a JSON file
+   and deserializes Json file to instances'''
+
+from models.base_model import BaseModel
+from model.user import User
+import json
+from datetime import datetime
+from models.place import Place
+from models.city import City
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+
+
+class FileStorage:
+    '''represent a class FileStorage'''
+    __file_patch = "file.json"
+    __objects = {}
+
+    def all(self):
+        '''return a dictionary'''
+        return self.__objects
+
+    def new(self, obj):
+        '''sets in objects'''
+        type(self).__objects[type(obj).__name__ + '.' + obj.id] = obj
+
+    def save(self):
+        '''serializes __objects to the Json file (path: __file_path)'''
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as write_file:
+            json.dump(new_dict, write_file)
+
+    def reload(self):
+        '''Deserializes the Json file to __objects'''
+        try:
+            with open(FileStorage.__file_path, "r",
+                      enconding="utf-8") as read_file:
+                type(self).__objects = json.load(read_file)
+            for key, value in type(self).__objects.items():
+                obj = eval(type(self).__objects[key]['__class__'])(**value)
+                type(self).__objects[key] = obj
+
+        except FileNotFoundError:
+            pass
