5ac408aa94aefa1aa76c63925cb5703bd96a99f2
Roberto <robpalacios1@hotmail.com>
2020-06-28 19:20:07 -0500

    updated task5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,19 +3,11 @@
    and deserializes Json file to instances'''
 
 from models.base_model import BaseModel
-from model.user import User
 import json
-from datetime import datetime
-from models.place import Place
-from models.city import City
-from models.state import State
-from models.review import Review
-from models.amenity import Amenity
-
 
 class FileStorage:
     '''represent a class FileStorage'''
-    __file_patch = "file.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -31,14 +23,14 @@ class FileStorage:
         new_dict = {}
         for key, value in FileStorage.__objects.items():
             new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w", encoding='utf-8') as write_file:
-            json.dump(new_dict, write_file)
+            with open(FileStorage.__file_path, "w",
+                encoding='utf-8') as write_file: json.dump(new_dict, write_file)
 
     def reload(self):
         '''Deserializes the Json file to __objects'''
         try:
             with open(FileStorage.__file_path, "r",
-                      enconding="utf-8") as read_file:
+                      encoding="utf-8") as read_file:
                 type(self).__objects = json.load(read_file)
             for key, value in type(self).__objects.items():
                 obj = eval(type(self).__objects[key]['__class__'])(**value)
