0c294b248a9c72a6b6f0bd9b96c44471022c28d5
Roberto <robpalacios1@hotmail.com>
2020-06-30 00:09:54 -0500

    add task5 Store first object

--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
+'''create a unique FileStorage instance for your application'''
 from models.engine.file_storage import FileStorage
 
-"""A variable storage, an instance of FileStorage"""
+'''A variable storage, an instance of FileStorage'''
 storage = FileStorage()
 storage.reload()
