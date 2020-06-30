dfafa715b3b6d57d5665ae49cd117c6fffbbed37
Roberto <robpalacios1@hotmail.com>
2020-06-30 01:28:59 -0500

    add task10 Console 1.0

--- console.py
+++ console.py
@@ -4,9 +4,15 @@
 import cmd
 import shlex
 import models
-from models.user import User
+import sys
 from models.base_model import BaseModel
-
+from models.engine.file_storage import FileStorage
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
