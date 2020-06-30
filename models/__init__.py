3e1fbe0ba2a13c3b72d3b8a960ade0169ed8c0ed
Roberto <robpalacios1@hotmail.com>
2020-06-30 12:03:32 -0500

    fix code by pep8

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''create a unique FileStorage instance'''
-
 from models.engine.file_storage import FileStorage
+
 
 '''A variable storage, an instance of FileStorage'''
 storage = FileStorage()
