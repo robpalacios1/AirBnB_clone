efd7e37df98423b52182946f9b0eb51b54bd8469
Roberto <robpalacios1@hotmail.com>
2020-06-30 00:55:00 -0500

    add place.py file

---
+++ models/place.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''class inherent of Base Model'''
+
+
+class Place(BaseModel):
+    '''class Place'''
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            super().__init__(**kwargs)
+        else:
+            super().__init__()
+
+        self.city_id = ""
+        self.user_id = ""
+        self.name = ""
+        self.description = ""
+        self.number_rooms = 0
+        self.number_bathrooms = 0
+        self.max_guest = 0
+        self.price_by_night = 0
+        self.latitude = 0.0
+        self.longitude = 0.0
+        self.amenity = []
