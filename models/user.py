e38936947b1e456edc44d2505f260f7018da7f51
Roberto <robpalacios1@hotmail.com>
2020-06-30 12:43:57 -0500

    fix shebang

--- models/user.py
+++ models/user.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 '''A class user that inherent from BaseModel'''
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     '''represent a class User'''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
     def __init__(self, *args, **kwargs):
+        '''initialize User'''
         if kwargs:
             super().__init__(**kwargs)
         else:
             super().__init__
-
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
