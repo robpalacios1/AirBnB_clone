4b33cf96b08831010c42647e1b294aa9250d9784
Roberto <robpalacios1@hotmail.com>
2020-06-30 12:43:06 -0500

    fix shebang

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 '''class inherent of BaseModel'''
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     '''class amenity'''
+    name = ""
+
     def __init__(self, *args, **kwargs):
+        '''initialize Amenity'''
         if kwargs:
             super().__init__(**kwargs)
         else:
             super().__init__
-
-        self.name = ""
