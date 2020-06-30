c62e3035a5f5b4d050749afa37a79c5e7b50a235
Roberto <robpalacios1@hotmail.com>
2020-06-29 23:13:39 -0500

    add task 8

--- console.py
+++ console.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 '''Method Command Interpreter'''
-​
 import cmd
 import shlex
 import models
 from models.user import User
 from models.base_model import BaseModel
-​
-​
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
     classes = {"BaseModel"}
