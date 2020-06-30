fecc8757257a6fd959080e0c2eb5d97dfa32f491
Roberto <robpalacios1@hotmail.com>
2020-06-30 12:43:46 -0500

    fix shebang

--- models/state.py
+++ models/state.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 '''class inherent of BaseModel'''
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
     '''class State'''
+    name = ""
+
     def __init__(self, *args, **kwargs):
+        '''initialize State'''
         if kwargs:
             super().__init__(**kwargs)
         else:
             super().__init__
-
-        self.name = ""
