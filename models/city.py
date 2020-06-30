aa08861a89a995ef855f064bba384de20c91bde1
Roberto <robpalacios1@hotmail.com>
2020-06-30 12:43:16 -0500

    fix shebang

--- models/city.py
+++ models/city.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 '''class inherent of BaseModel'''
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
     '''class city'''
+    state_id = ""
+    name = ""
+
     def __init__(self, *args, **kwargs):
+        '''initialize City'''
         if kwargs:
             super().__init__(**kwargs)
         else:
             super().__init__
-
-        self.stateid = ""
-        self.name = ""
