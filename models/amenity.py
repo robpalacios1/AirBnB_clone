fde8af36f1608c5bcfda51e41a17a09d4be144ac
Roberto <robpalacios1@hotmail.com>
2020-07-01 00:49:58 -0500

    fix code inside emacs

--- models/amenity.py
+++ models/amenity.py
@@ -5,9 +5,5 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     '''class amenity'''
-    name = ""
 
-    def __init__(self, *args, **kwargs):
-        '''initialize Amenity'''
-        if kwargs:
-            super().__init__(*args, **kwargs)
+    name = ""
