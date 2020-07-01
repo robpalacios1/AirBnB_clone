e08d0bdeff3431dc8c3b446c8b9b956f101a9dfa
Roberto <robpalacios1@hotmail.com>
2020-07-01 00:50:30 -0500

    fix code inside emacs

--- models/review.py
+++ models/review.py
@@ -5,11 +5,7 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     '''class Review'''
+
     place_id = ""
     user_id = ""
     text = ""
-
-    def __init__(self, *args, **kwargs):
-        '''initialize Review'''
-        if kwargs:
-            super().__init__(*args, **kwargs)
