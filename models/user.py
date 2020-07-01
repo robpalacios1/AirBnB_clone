b1854ccc0d2f165df71a3d9d5ece2cf8c386b970
Roberto <robpalacios1@hotmail.com>
2020-07-01 00:50:59 -0500

    fix code inside emacs

--- models/user.py
+++ models/user.py
@@ -5,12 +5,8 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     '''represent a class User'''
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        '''initialize User'''
-        if kwargs:
-            super().__init__(*args, **kwargs)
