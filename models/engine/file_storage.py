224321bed3d7039f88b498c5fb4579ccea7b569e
Roberto <robpalacios1@hotmail.com>
2020-06-30 20:34:19 -0500

    fix code inside emacs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,9 +10,6 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
 
 class FileStorage:
     '''Represents a class FileStorage'''
