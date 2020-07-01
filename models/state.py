a533273ba6e0fbfd7a52a2b217d5e8bed4d1c273
Roberto <robpalacios1@hotmail.com>
2020-07-01 00:50:40 -0500

    fix code inside emacs

--- models/state.py
+++ models/state.py
@@ -5,9 +5,5 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     '''class State'''
-    name = ""
 
-    def __init__(self, *args, **kwargs):
-        '''initialize State'''
-        if kwargs:
-            super().__init__(*args, **kwargs)
+    name = ""
