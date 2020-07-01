a2c47512526447e40488f62fe0adbae47bf4ed23
Roberto <robpalacios1@hotmail.com>
2020-06-30 23:10:23 -0500

    fix code inside emacs

--- models/place.py
+++ models/place.py
@@ -13,8 +13,8 @@ class Place(BaseModel):
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
-    latitude = 0
-    longitude = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
 
     def __init__(self, *args, **kwargs):
